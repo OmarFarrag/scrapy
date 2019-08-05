3331867e811e84d5e11a798f2fcaa6c8dac911c5
OmarFarrag <omar.alaa.farrag@gmail.com>
2019-08-05 17:28:24 +0200

    Adding steps to alg.

--- scrapy/utils/mime.py
+++ scrapy/utils/mime.py
@@ -52,10 +52,22 @@ class MimeTypes(object):
         ([0x3C ,0x21 ,0x2D ,0x2D ,0x3E], [0xFF,0xFF,0xFF,0xFF,0xFF], _WHITESSPACE_BYTES, 'text/html'),
         ([0x3C ,0x3F ,0x78 ,0x6D ,0x6C], [0xFF,0xFF,0xFF,0xFF,0xFF], _WHITESSPACE_BYTES, 'text/xml'),
         ([0x25 ,0x50 ,0x44 ,0x46 ,0x2D], [0xFF,0xFF,0xFF,0xFF,0xFF], [], 'application/pdf'),
-        ([0x25 ,0x21 ,0x50 ,0x53, 0x2D, 0x41, 0x64, 0x6F, 0x62 ,0x65 ,0x2D], [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], [], 'application/postscript'),
+        ([0x25 ,0x21 ,0x50 ,0x53, 0x2D, 0x41, 0x64, 0x6F, 0x62 ,0x65 ,0x2D], 
+        [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], [], 'application/postscript'),
         ([0xFE ,0xFF ,0x00 ,0x00], [0xFF ,0xFF ,0x00 ,0x00], [], 'text/plain'),
         ([0xFF ,0xFE ,0x00 ,0x00], [0xFF ,0xFF ,0x00 ,0x00], [], 'text/plain'),	
         ([0xEF ,0xBB ,0xBF ,0x00], [0xFF ,0xFF ,0xFF ,0x00], [], 'text/plain'),
+    ]
+    _BINARY_DATA_TYPES = [
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x0B, 0x0E, 0x0F,
+        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1C,
+        0x1D, 0x1E, 0x1F
+    ]
+    _TYPES_TO_CHECK_APACHE_BUG = [
+        'text/plain',
+        'text/plain; charset=ISO-8859-1',
+        'text/plain; charset=iso-8859-1',
+        'text/plain; charset=UTF-8'
     ]
     _EXTENSIONS = {
         'text/html': '.html',
@@ -77,26 +89,147 @@ class MimeTypes(object):
         'application/pdf': '.pdf'
     }
 
-    def from_response(self, response):
+    def from_response(self, response, no_sniff_flag = False ):
         """ Returns the MIME type for a response
         """
+        self.no_sniff_flag = no_sniff_flag
+        # 1
         protocol = response.url.split(':')[0]
         supplied_mime = self.get_supplied_mime(protocol,response.headers)
         if supplied_mime is None or supplied_mime.split(';')[0] in ['unkonown/unknown', 'application/unknown', '*/*']:
-            computed_mime = self._compute_mime_type(response.body, getattr(response, 'encoding', None))
-            return computed_mime
-        else:
+            self._sniff_scriptable_flag = not self.no_sniff_flag
+            return self._compute_mime_type(response.body, getattr(response, 'encoding', None))
+        # 2
+        if self.no_sniff_flag:
             return supplied_mime
+        #3
+        if self._check_for_apache_bug_flag:
+            return self._text_or_binary(response)
+        #4
+        if self._is_xml_mime_type(supplied_mime):
+            return supplied_mime
+        #5
+        if supplied_mime.split(';')[0] == "text/html":
+            return self._feed_or_html(response, supplied_mime)
+        return supplied_mime
+        
+
+    def _is_xml_mime_type(self, type):
+        return True if self._is_mime_type(type) and\
+            re.match(".*\+xml|text/xml|application/xml", type) else False
+    
+    def _feed_or_html(self, resource ,supplied_mime):
+        """Determine whether a feed has been mislabeled as HTML, and
+        returns the appropriate MIME type
+        """
+        sequence = self._get_resource_header(resource)
+        length = len(sequence)
+        s = 0
+        if length >= 3 and sequence[:3] == bytearray([0xEF, 0xBB, 0xBF]):  
+            s += 3
+        while s < length:
+            try:
+                while True:
+                    if sequence[s] == to_bytes('<'): # 0x3C
+                        s+=1
+                        break
+                    if sequence[s] not in _WHITESSPACE_BYTES:
+                        return supplied_mime
+                    s += 1
+                    
+                while True:   
+                    break_outer = False   
+                    if length >= s+3 and sequence[s:s+3] == to_bytes('!--'): # 0x21 0x2D 0x2D
+                        s += 3
+                        while True:
+                            if length >= s+3 and sequence[s:s+3] == to_bytes('-->'): # 0x2D 0x2D 0x3E
+                                s += 3
+                                break_outer = True
+                                break
+                            s += 1            
+                    if break_outer: break
+
+                    if length >= s+1 and sequence[s] == to_bytes('!') : # 0x21
+                        s += 1
+                        while True:
+                            if length >= s+1 and sequence[s] == to_bytes('>'): # 0x3E
+                                s += 1
+                                break_outer = True
+                                break
+                            s += 1
+                    if break_outer: break
+
+                    if length >= s+1 and sequence[s] == to_bytes('?'): # "?"
+                        s += 1
+                        while True:
+                            if length >= s+2 and sequence[s:s+2] == to_bytes('?>'): # 0x3F 0x3E
+                                s += 2
+                                break_outer = True
+                                break
+                            s += 1
+                    if break_outer: break
+
+                    if length >= s+3 and sequence[s:s+3] == to_bytes('rss'):  # 0x72 0x73 0x73
+                        return "application/rss+xml"
+
+                    if length >= s+4 and sequence[s:s+4] == to_bytes('feed'):  # 0x66 0x65 0x6 0x64
+                        return "application/atom+xml"
+
+                    if length >= s+7 and sequence[s:s+7] == to_bytes('rdf:RDF'): # 0x72 0x64 0x66 0x3A 0x52 0x44 0x46  
+                        s += 7   
+                        while True:  
+                            if length >= s+24 and sequence[s:s+24] == to_bytes('http://purl.org/rss/1.0/'):
+                                s += 24
+                                while True:
+                                    if length >= 43 and sequence[s:s+43] == to_bytes('http://www.w3.org/1999/02/22-rdf-syntax-ns#'):
+                                        return 'application/rss+xml'
+                                    s += 1
+                            if length >= 24 and sequence[s:s+24] == to_bytes('http://www.w3.org/1999/02/22-rdf-syntax-ns#'):
+                                s += 24
+                                while True:
+                                    if length >= 43 and sequence[s:s+43] == to_bytes('"http://purl.org/rss/1.0/"'):
+                                        return 'application/rss+xml'
+                                    s += 1
+                            s += 1
+                        
+                    return supplied_mime
+            except IndexError:
+                return supplied_mime             
+        return supplied_mime       
+               
+
+    def _text_or_binary(self, resource):
+        """ Determine whether a binary resource has been mislabeled as plain text
+        """
+        header_bytes = self._get_resource_header(resource)
+        length = len(header_bytes)
+        if (length >= 2 and header_bytes in [[0xFE,0xFF], [0xFF,0xFE]] or
+            length >= 3 and header_bytes in [[0xEF,0xBB, 0xBF], [0xFF,0xFE]] or
+            len([x for x in header_bytes if x in _BINARY_DATA_TYPES]) == 0):
+            # Conditions written in same sequence as mentioned in the standard
+            return 'text/plain'
+        return 'application/octet-stream'
+        
+
 
     def get_supplied_mime(self, protocol, headers=None):
         supplied_type = None
         if protocol.upper() in ['HTTP', 'HTTPS'] and headers is not None:
             supplied_type = headers.get('Content-Type')
+            self._check_for_apache_bug(supplied_type)
         # TODO: Add support here for other protocols 
         if self._is_mime_type(supplied_type):
             return supplied_type
         else:
             return None
+
+    def _check_for_apache_bug(self, type):
+        """ Checks if the MIME type extracted from headers is a one that 
+        might be mistakenly set as a result of Apache's bug
+        """
+        self._check_for_apache_bug_flag = False
+        if type in _TYPES_TO_CHECK_APACHE_BUG:
+            self._check_for_apache_bug_flag = True
 
     def _is_mime_type(self, type):
         """ Checks that a given string is a valid MIME type string.
